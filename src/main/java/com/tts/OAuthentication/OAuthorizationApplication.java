c5bd339a9aa9acbfc3d45dfd51c01874c743fb18
Jordan <71449357+sbjordan222@users.noreply.github.com>
2021-03-07 22:27:20 -0600

    first commit

---
+++ src/main/java/com/tts/OAuthentication/OAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.tts.OAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/OAuthentication/OAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.OAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
